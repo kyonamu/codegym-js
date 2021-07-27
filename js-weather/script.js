7851b8349404a62184a263762c0f32911713f689
小川　木楠 <>
2021-07-27 20:28:01 +0900

    jsの編集

--- js-weather/script.js
+++ js-weather/script.js
@@ -3,23 +3,14 @@ const des = document.getElementById("weather");
 const API_KEY = "4b5774e9f3d2a07b84f0f2f88e486224";
 const btn = document.getElementById("btn");
 
-addEventListener("load", async function getWeatherData() {
+const sampleAsyncFunc = async () => {
   const city = area.value;
   const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
   const weather = await res.json();
-  console.log(weather);
   const inner = weather.weather[0]['description'];
   des.innerHTML = `現在の天気は${inner}です`;
-});
+}
 
-btn.addEventListener("click", async function getWeatherData() {
-  const city = area.value;
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
-  const weather = await res.json();
-  console.log(weather);
-  const inner = weather.weather[0]['description'];
-  des.innerHTML = `現在の天気は${inner}です`;
-});
+addEventListener("load", sampleAsyncFunc);
 
-
-getWeatherData();
+btn.addEventListener("click", sampleAsyncFunc);
